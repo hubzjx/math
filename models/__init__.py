d53cfdd49211ac339af4a254fe4478384f8727be
Zhang Jx <97077032+hubzjx@users.noreply.github.com>
2025-11-06 17:53:04 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-# This makes the models directory a Python package
-from .positioning_models import UAVPosition, AngleMeasurement, CircularFormationModel, ConeFormationModel
-
-__all__ = ['UAVPosition', 'AngleMeasurement', 'CircularFormationModel', 'ConeFormationModel']
