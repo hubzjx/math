0b34631d2821b55689c39efaa455f4fc6c98c408
Zhang Jx <97077032+hubzjx@users.noreply.github.com>
2025-11-06 17:53:27 +0800

    Delete visualization directory

--- visualization/__init__.py
+++
@@ -1,4 +0,0 @@
-# This makes the visualization directory a Python package
-from .formation_plots import FormationVisualizer, create_mind_map_visualization
-
-__all__ = ['FormationVisualizer', 'create_mind_map_visualization']
