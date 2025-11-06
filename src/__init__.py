08bf82b308d2cdad00b15070963bf58289b558c5
Zhang Jx <97077032+hubzjx@users.noreply.github.com>
2025-11-06 17:53:18 +0800

    Delete src directory

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-# This makes the src directory a Python package
-from .positioning_algorithms import UAVPositioningAlgorithms, Problem1CSolver
-
-__all__ = ['UAVPositioningAlgorithms', 'Problem1CSolver']
